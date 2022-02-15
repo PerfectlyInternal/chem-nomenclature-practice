42470c760978d9da220fa0e7a595ccc2d0908a51
PerfectlyInternal <55606714+PerfectlyInternal@users.noreply.github.com>
2022-02-15 13:28:26 -0500

    started generation of compounds

---
+++ main.py
@@ -0,0 +1,15 @@
+import random
+import element
+import compound
+
+# element groups
+ALKALI_METALS = ()
+ALKALINE_EARTH_METALS = ()
+TRANSITION_METALS = ()
+METALLOIDS = ()
+NONMETALS = ()
+HALOGENS = ()
+
+# generate random compound
+def generate_compound():
+	pass
