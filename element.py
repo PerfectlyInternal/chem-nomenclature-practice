47747b60fe659277b4e8d0654dde4b2100247ab4
PerfectlyInternal <55606714+PerfectlyInternal@users.noreply.github.com>
2022-02-15 15:58:40 -0500

    added mass + oxy states

--- element.py
+++ element.py
@@ -1,7 +1,11 @@
 class element:
 	name = ""
 	symbol = ""
+	mass = 0.0
+	oxidation_states = [0]
 	
-	def element(self, _name, _symbol):
-		self.name = _name;
+	def element(self, _name, _symbol, _mass, _oxidation_states):
+		self.name = _name
 		self.symbol = _symbol
+		self.mass = _mass
+		self.oxidation_states = oxidation_states
