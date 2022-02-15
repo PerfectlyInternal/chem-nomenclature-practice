800a602de1831ace50fa69df80311471e0e4320b
PerfectlyInternal <55606714+PerfectlyInternal@users.noreply.github.com>
2022-02-15 16:11:52 -0500

    name calculation from formula

--- compound.py
+++ compound.py
@@ -1,3 +1,5 @@
+PREFIXES = {1:"mono", 2:"di", 3:"tri", 4:"tetra", 5:"penta", 6:"hexa", 7:"hepta", 8:"octo", 9:"nona", 10:"deca"}
+
 class compound:
 	formula = {}
 	charge = 0
@@ -9,4 +11,8 @@ class compound:
 		name = calculate_name(_formula)
 	
 	def calculate_name(self, formula):
-		return "COMPOUND_NAME"
+		compound_name = ""
+		for element in formula.keys():
+			# add the element's name to the name with its prefix
+			compound_name += PREFIXES[formula[element]] + " "
+		return compound_name
